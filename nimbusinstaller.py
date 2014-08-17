0a7128d24d3fb73333af4541e6fd07f65e19c5b2
ihavegerms <ihavegerms@users.noreply.github.com>
2014-08-17 18:04:10 -0500

    Update nimbusinstaller.py
    pep8 compliant

--- nimbusinstaller.py
+++ nimbusinstaller.py
@@ -1,24 +1,23 @@
-#!/usr/bin/python
-
 import subprocess
 import socket
-import os
 import re
 
+
 def main():
+
     reqchk()
 
-def ipcollect():
 
-    # this function collects the internal ip's to install nimbus on.
-    # *add later* use the ip addresses to confirm hostnames via /etc/hosts
+def ipcollect():
+  # this function collects the internal ip's to install nimbus on.
+  # *add later* use the ip addresses to confirm hostnames via /etc/hosts
 
     valid = []
     invalid = []
 
     x = raw_input("internal ip's to install Nimbus on [space seperated]: ")
 
-    # uses spaces to split input and performs basic checks to confirm accuracy
+  # uses spaces to split input and performs basic checks to confirm accuracy
     splitx = x.split()
 
     for splitip in splitx:
@@ -30,9 +29,9 @@ def ipcollect():
             valid.append(splitip)
     return (valid)
 
-def reqchk():
 
-    # instantiate list from ipcollect function and write to a dsh group
+def reqchk():
+  # instantiate list from ipcollect function and write to a dsh group
     ips = ipcollect()
     reqchk_group = open('/etc/dsh/group/reqchk', 'w')
     for ip in ips:
@@ -40,18 +39,21 @@ def reqchk():
         reqchk_group.write("\n")
     reqchk_group.close()
 
-    # confirm required files exist on each host
-    doiexist = ["/root/.rackspace/server_number", "/root/.rackspace/datacenter", "/root/.rackspace/customer_number"]
+  # confirm required files exist on each host
+    doiexist = ["/root/.rackspace/server_number",
+                "/root/.rackspace/datacenter",
+                "/root/.rackspace/customer_number"]
 
     for ip in ips:
         loopcnt = 0
         for path in doiexist:
             gather = subprocess.call(["ssh", ip, "cat " + path])
-            verify = re.sub(r'^0', '\n', str(gather))
+            re.sub(r'^0', '\n', str(gather))
             loopcnt += 1
 
             if loopcnt == 3:
                 print "---"
+
 
 if __name__ == "__main__":
     main()
