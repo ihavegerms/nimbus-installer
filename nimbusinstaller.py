2c61240d453e3c33a4ecf8c96e95aaee0b0c0763
ihavegerms <ihavegerms@users.noreply.github.com>
2014-08-17 22:10:08 -0500

    Update nimbusinstaller.py
    added install() function. testing against lab environment looks successful... but i still need to run this against an environment where nimbus is not installed, as well as write in a cleanup to remove the mess made.

--- nimbusinstaller.py
+++ nimbusinstaller.py
@@ -2,6 +2,7 @@ import subprocess
 import socket
 import re
 
+x = raw_input("internal ip's to install Nimbus on [space seperated]: ")
 
 def main():
 
@@ -11,11 +12,9 @@ def main():
 def ipcollect():
   # this function collects the internal ip's to install nimbus on.
   # *add later* use the ip addresses to confirm hostnames via /etc/hosts
-
+    global x
     valid = []
     invalid = []
-
-    x = raw_input("internal ip's to install Nimbus on [space seperated]: ")
 
   # uses spaces to split input and performs basic checks to confirm accuracy
     splitx = x.split()
@@ -54,6 +53,30 @@ def reqchk():
             if loopcnt == 3:
                 print "---"
 
+    proceed = raw_input("Is the above information correct (y/n): ")
+    print ""
+    if proceed.lower() == "y":
+        print "[+] proceeding with installation..."
+        install()
+    elif proceed.lower() == "n":
+        print "[-] exiting per request..."
+        exit
+
+def install():
+
+    ips = ipcollect()
+    print "[+] downloading/extracting/installing nimbus-installer.tar.gz"
+    for ip in ips:
+        subprocess.call(["wget", "--quiet", "http://nimbus.howopenstack.org/nimbus-installer.tar.gz",
+                         "-O", "/tmp/nimbus-installer.tar.gz"])
+        subprocess.call(["wget", "--quiet", "--no-check-certificate",
+                         "https://raw.githubusercontent.com/rcbops/support-tools/master/support-scripts/rax-nimbus-installer.sh",
+                         "-qO", "/tmp/rax-nimbus-installer.sh"])
+        subprocess.call(["bash", "/tmp/rax-nimbus-installer.sh"])
+    print '[+] installation complete. verify openstack probes' \
+          ' in /opt/nimsoft/probes/'
+    print "[+] this process could take up to 15 minutes"
+    print ""
 
 if __name__ == "__main__":
     main()
